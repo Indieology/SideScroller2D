351092c909b4459ed1efcbc77472f1d23dbda5b8
Indieology <contactindieology@gmail.com>
2023-02-28 08:46:33 -0600

    Add states for shooting while running and falling
    Added the shoot state, and the shoot_fall state. Updated the player animations while entering these states. Made is so the player can shoot at any time during their 8 frame rune cycle, forcing me to create 8 different shooting animations that can begin at any one of the 8 frames of the player run cycle. Created a timer to keep track of when the state machine should leave a shooting state.

--- Player/Player.gd
+++ Player/Player.gd
@@ -7,11 +7,14 @@ const JUMP_VELOCITY = -275.0
 var gravity = 986
 var direction = Vector2.ZERO
 
-enum states {IDLE, RUN, JUMP, FALL, SLIDE}
+enum states {IDLE, RUN, JUMP, FALL, SLIDE, SHOOT, SHOOT_FALL}
 var current_state = states.IDLE
+
+var last_shoot_animation = "Shoot1"
 
 func _ready():
 	$SlideTimer.stop()
+	$ShootTimer.stop()
 	current_state = states.RUN
 
 func _physics_process(delta):
@@ -21,20 +24,28 @@ func _physics_process(delta):
 	#direction = Input.get_axis("ui_left", "ui_right")
 	direction = 1
 	
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		current_state = states.JUMP
-	elif velocity.y > 15:
-		current_state = states.FALL
-	elif direction and current_state != states.JUMP and $SlideTimer.is_stopped():
+	elif velocity.y > 15 and $ShootTimer.is_stopped():
+		if Input.is_action_just_pressed("shoot"):
+			current_state = states.SHOOT_FALL
+		else:
+			current_state = states.FALL
+	elif Input.is_action_just_pressed("shoot"):
+		if current_state == states.JUMP:
+			current_state = states.SHOOT_FALL
+		else:
+			current_state = states.SHOOT
+	elif direction and current_state != states.JUMP and $SlideTimer.is_stopped() and $ShootTimer.is_stopped():
 		current_state = states.RUN
-		if Input.is_action_just_pressed("ui_down"):
+		if Input.is_action_just_pressed("slide"):
 			current_state = states.SLIDE
-	elif $JumpTimer.is_stopped() and $SlideTimer.is_stopped():
+	elif $JumpTimer.is_stopped() and $SlideTimer.is_stopped() and $ShootTimer.is_stopped():
 		if direction:
 			current_state = states.RUN
 		else:
 			current_state = states.IDLE
-		if Input.is_action_just_pressed("ui_down"):
+		if Input.is_action_just_pressed("slide"):
 			current_state = states.SLIDE
 	run_current_state()
 	move_and_slide()
@@ -51,6 +62,31 @@ func run_current_state():
 			$Label.text = "RUN"
 			velocity.x = direction * SPEED
 			$AnimatedSprite2D.play("Run_Aim")
+			match last_shoot_animation:
+				"Shoot1":
+					$AnimatedSprite2D.set_frame_and_progress(4,0)
+					last_shoot_animation = "none"
+				"Shoot2":
+					$AnimatedSprite2D.set_frame_and_progress(5,0)
+					last_shoot_animation = "none"
+				"Shoot3":
+					$AnimatedSprite2D.set_frame_and_progress(6,0)
+					last_shoot_animation = "none"
+				"Shoot4":
+					$AnimatedSprite2D.set_frame_and_progress(7,0)
+					last_shoot_animation = "none"
+				"Shoot5":
+					$AnimatedSprite2D.set_frame_and_progress(0,0)
+					last_shoot_animation = "none"
+				"Shoot6":
+					$AnimatedSprite2D.set_frame_and_progress(1,0)
+					last_shoot_animation = "none"
+				"Shoot7":
+					$AnimatedSprite2D.set_frame_and_progress(2,0)
+					last_shoot_animation = "none"
+				"Shoot8":
+					$AnimatedSprite2D.set_frame_and_progress(3,0)
+					last_shoot_animation = "none"
 		states.JUMP:
 			$Label.text = "JUMP"
 			$AnimatedSprite2D.play("Jump")
@@ -67,7 +103,43 @@ func run_current_state():
 			$AnimatedSprite2D.play("Slide")
 			if $SlideTimer.time_left <= 0:
 				$SlideTimer.start()
-				
+		states.SHOOT:
+			velocity.x = direction * SPEED
+			$Label.text = "SHOOT"
+			if $ShootTimer.time_left <= 0:
+				$ShootTimer.start()
+				match $AnimatedSprite2D.frame:
+					0:
+						$AnimatedSprite2D.play("Shoot1")
+						last_shoot_animation = "Shoot1"
+					1:
+						$AnimatedSprite2D.play("Shoot2")
+						last_shoot_animation = "Shoot2"
+					2:
+						$AnimatedSprite2D.play("Shoot3")
+						last_shoot_animation = "Shoot3"
+					3:
+						$AnimatedSprite2D.play("Shoot4")
+						last_shoot_animation = "Shoot4"
+					4:
+						$AnimatedSprite2D.play("Shoot5")
+						last_shoot_animation = "Shoot5"
+					5:
+						$AnimatedSprite2D.play("Shoot6")
+						last_shoot_animation = "Shoot6"
+					6:
+						$AnimatedSprite2D.play("Shoot7")
+						last_shoot_animation = "Shoot7"
+					7:
+						$AnimatedSprite2D.play("Shoot8")
+						last_shoot_animation = "Shoot8"
+		states.SHOOT_FALL:
+			$Label.text = "SHOOT FALL"
+			if $ShootTimer.time_left <= 0:
+				$ShootTimer.start()
+				$AnimatedSprite2D.play("Shoot_Fall")
+			if is_on_floor():
+				current_state = states.RUN
 	if current_state == states.SLIDE:
 		$SlideCollisionShape.set_deferred("disabled", false)
 		$CollisionShape2D.set_deferred("disabled", true)
