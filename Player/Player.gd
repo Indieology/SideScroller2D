b67478315976ffdfc399aefa0d550eebba27f189
Indieology <contactindieology@gmail.com>
2023-02-28 04:54:49 -0600

    Force player to run to the right
    Player will always run to the right until defeated. Between runs the player will be able to move around and purchase upgrades

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 
-const SPEED = 125.0
+const SPEED = 115.0
 const JUMP_VELOCITY = -275.0
 
 var gravity = 986
@@ -14,7 +14,8 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
-	direction = Input.get_axis("ui_left", "ui_right")
+	#direction = Input.get_axis("ui_left", "ui_right")
+	direction = 1
 	
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		current_state = states.JUMP
@@ -30,6 +31,7 @@ func _physics_process(delta):
 	run_current_state()
 	move_and_slide()
 	#print(velocity)
+
 func run_current_state():
 	
 	match current_state:
